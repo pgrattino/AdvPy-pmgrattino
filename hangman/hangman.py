e4b72d53a91e3426eddb0ccb72b43916c9f51529
Peyton Grattino <grattinopeyton@gmail.com>
2022-09-01 22:02:23 -0600

    Added comments to the code

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -4,12 +4,12 @@ import random
 
 def words():
     for _ in range(1):
-        numOfWord = random.randint(1, 20)
-    wordBank = open("word.txt")
-    wordlines = wordBank.readlines()
-    useWord = wordlines[numOfWord - 1]
-    useWord = useWord.strip()
-    return useWord
+        numOfWord = random.randint(1, 20)  # generating random number
+    wordBank = open("word.txt")  # Opening text file
+    wordlines = wordBank.readlines()  # Storing the words in a list
+    useWord = wordlines[numOfWord - 1]  # Using random number to pick a word in the list
+    useWord = useWord.strip()  # Stripping the word of any extra characters
+    return useWord  # Return the word for the game
 
 
 print(words())
